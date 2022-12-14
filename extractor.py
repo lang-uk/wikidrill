3b62b6a24d72f28453b05fa13fc3778853724df3
romanyshyn-natalia <romanyshyn.n@ucu.edu.ua>
2022-12-14 18:25:16 +0200

    added code for relation pairs dataset creation

--- extractor.py
+++ extractor.py
@@ -14,6 +14,8 @@ from choppa.srx_parser import SrxDocument
 from choppa.iterators import SrxTextIterator
 import re
 import parse_wikidata
+import pandas as pd
+import wn
 
 ACUTE = chr(0x301)
 GRAVE = chr(0x300)
@@ -21,52 +23,57 @@ ruleset = Path(choppa.__file__).parent / "data/srx/languagetool_segment.srx"
 SRX_2_XSD = Path(choppa.__file__).parent / "data/xsd/srx20.xsd"
 document = SrxDocument(ruleset=ruleset, validate_ruleset=SRX_2_XSD)
 PATTERN = re.compile("\)\s—")
+PREFIX = "omw-en31"
 
 nlp = stanza.Pipeline(lang="uk", processors="tokenize,mwt,pos,lemma", verbose=False)
 
+COUNTER = 0
 
-# Todo: * data structure and traversal
 
 def remove_accents(s: str) -> str:
     return s.replace(ACUTE, "").replace(GRAVE, "")
 
 
-def lemmatize(s: str) -> List[str]:
-    doc = nlp(s)
-    return [word.lemma for sent in doc.sentences for word in sent.words]
+# def lemmatize(s: str) -> List[str]:
+#     doc = nlp(s)
+#     return [word.lemma for sent in doc.sentences for word in sent.words]
 
 
 def extract_from_page(word: str) -> Dict:
-    s = executor.execute("node", "./wtf", word, capture=True)
-    page = json.loads(s)
-
+    try:
+        s = executor.execute("node", "./wtf", word, capture=True)
+        page = json.loads(s)
+    except ValueError:
+        page = {"json": dict()}
     merged_sections = defaultdict(list)
     for info_sections in jmespath.search("json.sections[].infoboxes[]", page) or {}:
         for k, v in info_sections.items():
             if "text" in v:
                 merged_sections[k].append(v["text"])
 
-    sentences: List[str] = []
-    for s in page["plaintext"].splitlines():
-        if not s.strip():
-            continue
+    #     sentences: List[str] = []
+    #     for s in page["plaintext"].splitlines():
+    #         if not s.strip():
+    #             continue
 
-        for text in SrxTextIterator(document, "uk_one", s.strip(), max_lookbehind_construct_length=1024 * 10):
-            if not text:
-                continue
-            sentences.append(text)
+    #         for text in SrxTextIterator(document, "uk_one", s.strip(), max_lookbehind_construct_length=1024 * 10):
+    #             if not text:
+    #                 continue
+    #             sentences.append(text)
 
-    title = remove_accents(page["json"]["title"]).strip().lower()
-    filtered_sentences: List[str] = []
+    #     title = remove_accents(page["json"]["title"]).strip().lower()
+    #     filtered_sentences: List[str] = []
 
-    for sent in tqdm(sentences):
-        if title in lemmatize(sent):
-            filtered_sentences.append(sent)
+    #     for sent in tqdm(sentences):
+    #         if title in lemmatize(sent):
+    #             filtered_sentences.append(sent)
 
-    page["infoboxes"] = merged_sections
-    page["examples"] = filtered_sentences
-
-    gloss = jmespath.search("json.sections[0].paragraphs[].sentences[0].text", page)[0] or ""
+    #     page["infoboxes"] = merged_sections
+    #     page["examples"] = filtered_sentences
+    if jmespath.search("json.sections[0].paragraphs[].sentences[0].text", page):
+        gloss = jmespath.search("json.sections[0].paragraphs[].sentences[0].text", page)[0]
+    else:
+        gloss = ""
     if "—" in gloss:
         if re.search(PATTERN, gloss):
             _, gloss = re.split(PATTERN, gloss, 1)
@@ -77,31 +84,4 @@ def extract_from_page(word: str) -> Dict:
 
 
 if __name__ == "__main__":
-    # print(json.dumps(extract_from_page("Буряк"), indent=4, ensure_ascii=False))
-
-    # bfs to test
-    visited = []
-    queu = []
-    X = extract_from_page("жук")["wikidata_id"]
-    visited.append(X)
-    queu.append(X)
-    while queu:
-        t = queu.pop(0)
-        print(t)
-        if not parse_wikidata.get_pwn_id(t):
-            continue
-        hypernyms = parse_wikidata.get_hypernyms(parse_wikidata.get_pwn_id(t))
-        title_url = parse_wikidata.get_wiki_url(t)
-        if not title_url:
-            continue
-        title = extract_from_page(title_url)['json']['title']
-        print(title, ":", [extract_from_page(parse_wikidata.get_wiki_url(q))['json']['title'] for q in hypernyms if
-                           q and parse_wikidata.get_wiki_url(q)])
-        for x in hypernyms:
-            if x and x not in visited:
-                url = parse_wikidata.get_wiki_url(x)
-                if not url:
-                    continue
-                new_page = extract_from_page(url)
-                visited.append(x)
-                queu.append(x)
+    print(json.dumps(extract_from_page("Буряк"), indent=4, ensure_ascii=False))
--- parse_wikidata.py
+++ parse_wikidata.py
@@ -1,40 +1,72 @@
 from wikidata.client import Client
 import wn
 import mkwikidata
+import urllib.error
 
 # wn.download("omw-en31:1.4")
 
 WORDNET_PROPERTY = "P8814"
-PREFIX = "omw-en31"
+
+
+def get_hyponyms(synset_id):
+    """
+    Returns hyponyms for given synset id.
+    :param synset_id: string
+    :return: list
+    """
+    if "hyponym" not in wn.synset(synset_id).relations():
+        return []
+    return [el.id for el in wn.synset(synset_id).relations()["hyponym"]]
+
+
+def get_instance_hyponyms(synset_id):
+    """
+    Returns instance hyponyms for given synset id.
+    :param synset_id: string
+    :return: list
+    """
+    if "instance_hyponym" not in wn.synset(synset_id).relations():
+        return []
+    return [el.id for el in wn.synset(synset_id).relations()["instance_hyponym"]]
+
+
+def get_hypernyms(synset_id):
+    """
+    Returns hypernyms for given synset id.
+    :param synset_id: string
+    :return: list
+    """
+    if "hypernyms" not in wn.synset(synset_id).relations():
+        return []
+    return [el.id for el in wn.synset(synset_id).relations()["hypernyms"]]
 
 
 def get_pwn_id(wikidata_id):
     client = Client()
     pwn_prop = client.get(WORDNET_PROPERTY)
-    parent_entity = client.get(wikidata_id, load=True)
+    try:
+        parent_entity = client.get(wikidata_id, load=True)
+    except urllib.error.URLError as e:
+        parent_entity = e.read().decode("utf8", 'ignore')
+
     return parent_entity.get(pwn_prop)
-
-
-def get_hypernyms(pwn_id):
-    return {get_wikidata_id(el.id): el.lemmas() for el in wn.synset(f"{PREFIX}-{pwn_id}").hypernyms() if
-            wn.synset(f"{PREFIX}-{pwn_id}")}
 
 
 def parse_synset_id(s_id):
     return "-".join(s_id.split('-')[-2:])
 
 
-def get_hyponyms(pwn_id):
-    return {get_wikidata_id(parse_synset_id(el.id)): el.lemmas() for el in wn.synset(f"{PREFIX}-{pwn_id}").hyponyms()}
-
-
 def get_wikidata_id(synset_id):
     query = build_query(parse_synset_id(synset_id))
-    result = mkwikidata.run_query(query)['results']['bindings']
-    if result:
-        return result[0]['itemLabel']['value']
-    print(f"Not found wikidata id for {synset_id}")
-    return ""
+    try:
+        result = mkwikidata.run_query(query)['results']['bindings']
+        if result:
+            return result[0]['itemLabel']['value']
+        else:
+            print(f"Not found wikidata id for {synset_id}")
+            return ""
+    except ValueError:
+        return ""
 
 
 def build_query(wordnet_id):
